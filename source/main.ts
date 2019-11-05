7df01e536a1d29674565437c4484710c8f9b597c
Doverstav <pontus.doverstav@kits.se>
2019-11-05 13:03:18 +0100

    Remove a bunch of old classes and tests

--- source/main.ts
+++ source/main.ts
@@ -1,5 +1,3 @@
-console.log("hello world!");
-
 interface Graph {
     nodes: Vertex[];
 
@@ -12,62 +10,6 @@ interface Vertex {
 
     addEdge(neighbour: Vertex);
 }
-
-class SimpleGraph implements Graph {
-    nodes: Vertex[];
-
-    constructor(initialNodes: Vertex[]) {
-        this.nodes = initialNodes;
-    }
-
-    addNode(node: Vertex) { }
-
-    toString(): string {
-        let textGraph = "";
-
-        this.nodes.forEach(node => {
-            let textNeighbours = node.neighbours
-                .map(neighbour => neighbour.id)
-                .reduce((acc, id) => acc + `${id}, `, "");
-
-            textGraph = textGraph.concat(`${node.id}: { ${textNeighbours} }\n`);
-        });
-
-        return textGraph;
-    }
-}
-
-class SimpleNode implements Vertex {
-    id: string;
-    neighbours: Vertex[];
-
-    constructor(id: string, initialNeighbours: Vertex[]) {
-        this.id = id;
-        this.neighbours = initialNeighbours;
-    }
-
-    addEdge(neighbour: Vertex) {
-        this.neighbours.push(neighbour);
-    }
-}
-
-/*
-let node1 = new SimpleNode('1', []);
-let node2 = new SimpleNode('2', []);
-let node3 = new SimpleNode('3', []);
-let node4 = new SimpleNode('4', []);
-let node5 = new SimpleNode('5', []);
-let node6 = new SimpleNode('6', []);
-
-node1.addEdge(node2);
-node1.addEdge(node3);
-node3.addEdge(node4);
-node4.addEdge(node5);
-
-let graph = new SimpleGraph([node1, node2, node3, node4]);
-
-console.log(graph.toString());
-*/
 
 interface WeightedVertex {
     id: string;
@@ -84,115 +26,6 @@ interface Edge {
     cost: number;
 }
 
-class WeightedGraph {
-    nodes: WeightedNode[];
-
-    constructor(initialNodes: WeightedNode[]) {
-        this.nodes = initialNodes;
-    }
-    
-    addNode(node: WeightedNode) {
-        throw new Error("Method not implemented.");
-    }
-
-    toString(): string {
-        let textGraph = "";
-
-        this.nodes.forEach(node => {
-            let textNeighbours = node.edges
-                .reduce((acc, edge: Edge) => acc + ` -${edge.cost}-> ${edge.to.id} `, "");
-
-            textGraph = textGraph.concat(`${node.id}: { ${textNeighbours} }\n`);
-        });
-
-        return textGraph;
-    }
-}
-
-class WeightedNode implements WeightedVertex {
-    id: string;
-    edges: Edge[];
-
-    constructor(id: string, initialEdges: Edge[]) {
-        this.id = id;
-        this.edges = initialEdges;
-    }
-
-    addEdge(neighbour: WeightedVertex, cost: number) {
-        this.edges.push(new SimpleEdge(this, neighbour, cost));
-    }
-    
-    getEdges() {
-        return this.edges;
-    }
-
-    equals(toCompare: WeightedVertex) {
-        return this.id === toCompare.id;
-    }
-}
-
-// Useless?!?!
-class WorldGraph {
-    nodes: WorldNode[];
-
-    constructor(initialNodes: WorldNode[]) {
-        this.nodes = initialNodes;
-    }
-    
-    addNode(node: WorldNode) {
-        throw new Error("Method not implemented.");
-    }
-
-    toString(): string {
-        let textGraph = "";
-
-        this.nodes.forEach(node => {
-            let textNeighbours = node.edges
-                .reduce((acc, edge: Edge) => acc + ` -${edge.cost}-> ${edge.to.id} `, "");
-
-            textGraph = textGraph.concat(`${node.id}: { ${textNeighbours} }\n`);
-        });
-
-        return textGraph;
-    }
-}
-
-class WorldNode implements WeightedVertex {
-    id: string;
-    edges: Edge[];
-    worldState: SimpleWorld;
-
-    constructor(id: string, initialEdges: Edge[], initialState: SimpleWorld) {
-        this.id = id;
-        this.edges = initialEdges;
-        this.worldState = initialState;
-    }
-
-    getEdges() {
-        if(this.edges.length === 0) {
-            this.generateEdges()
-        }
-        return this.edges;
-    }
-
-    generateEdges() {
-        let validNextStates = this.worldState.generateValidNextStates();
-
-        validNextStates.forEach( worldState => {
-            let createdNeighbour = new WorldNode((Math.random() * 100000).toString(), [], worldState);
-            this.edges.push(new SimpleEdge(this, createdNeighbour, 1));
-        });
-    }
-
-    addEdge(neighbour: WorldNode, cost: number) {
-        this.edges.push(new SimpleEdge(this, neighbour, cost));
-    }
-
-    equals(toCompare: WorldNode) {
-        return this.worldState.equals(toCompare.worldState);
-    }
-}
-
 class SimpleEdge implements Edge {
     from: WeightedVertex;
     to: WeightedVertex;
@@ -202,74 +35,6 @@ class SimpleEdge implements Edge {
         this.from = from;
         this.to = to;
         this.cost = cost;
-    }
-}
-
-class SimpleWorld {
-    position: {x: number, y: number};
-    map: (string []) [];
-
-    constructor(map: string [] [], startingPosition: {x:number, y:number}) {
-        this.map = map;
-        this.position = startingPosition;
-    }
-
-    generateValidNextStates(): SimpleWorld [] {
-        let allValidNextStates: SimpleWorld [] = [];
-        let currentX = this.position.x;
-        let currentY = this.position.y;
-        
-        // Check position to right
-        if(currentX + 1 < this.map[currentY].length) {
-            // We are in bounds
-            if(this.isRoom(currentX + 1, currentY)) {
-                // Next position is room and thus valid
-                allValidNextStates.push(new SimpleWorld(this.map, {x: currentX + 1, y: currentY}));
-            }
-        }
-
-        // Check position to left
-        if(currentX - 1 >= 0) {
-            // We are in bounds
-            if(this.isRoom(currentX - 1, currentY)) {
-                // Next position is room and thus valid
-                allValidNextStates.push(new SimpleWorld(this.map, {x: currentX - 1, y: currentY}));
-            }
-        }
-
-        // Check position below
-        if(currentY + 1 < this.map.length) {
-            // We are in bounds
-            if(this.isRoom(currentX, currentY + 1)) {
-                // Next position is room and thus valid
-                allValidNextStates.push(new SimpleWorld(this.map, {x: currentX, y: currentY + 1}));
-            }
-        }
-
-        // Check position above
-        if(currentY - 1 >= 0) {
-            // We are in bounds
-            if(this.isRoom(currentX, currentY - 1)) {
-                // Next position is room and thus valid
-                allValidNextStates.push(new SimpleWorld(this.map, {x: currentX, y: currentY - 1}));
-            }
-        }
-
-        return allValidNextStates;
-    }
-
-    private isRoom(x: number, y: number) {
-        let room = this.getRoom(x,y);
-        return room === 'r';
-    }
-
-    private getRoom(x: number, y: number) {
-        return this.map[y][x];
-    }
-
-    equals(toCompare: SimpleWorld) {
-        return this.position.x === toCompare.position.x
-                && this.position.y === toCompare.position.y;
     }
 }
 
@@ -330,7 +95,7 @@ class MapObject {
     }
 }
 
-class Robot implements WeightedNode {
+class Robot implements WeightedVertex {
     id: string;
     edges: Edge[];
 
@@ -577,98 +342,6 @@ function printPath(path: Edge []) {
     return stringPath;
 }
 
-function printWorldPath(path: Edge[]) {
-    let stringPath = "";
-    for(let i = 0; i < path.length; i++) {
-        let from = (path[i].from as WorldNode);
-
-        stringPath = stringPath.concat(`{ x: ${from.worldState.position.x}, y: ${from.worldState.position.y} } -${path[i].cost}-> `);
-        if(i === path.length - 1) {
-            let to = (path[i].to as WorldNode);
-            stringPath = stringPath.concat(`{ x: ${to.worldState.position.x}, y: ${to.worldState.position.y} }`);
-        }
-    }
-    return stringPath;
-}
-
-let node1 = new WeightedNode('1', []);
-let node2 = new WeightedNode('2', []);
-let node3 = new WeightedNode('3', []);
-let node4 = new WeightedNode('4', []);
-let node5 = new WeightedNode('5', []);
-let node6 = new WeightedNode('6', []);
-
-node1.addEdge(node2, 4);
-node1.addEdge(node3, 2);
-node3.addEdge(node2, 1)
-node3.addEdge(node4, 10);
-node4.addEdge(node5, 1);
-
-let graph = new WeightedGraph([node1, node2, node3, node4, node5]);
-
-console.log(graph.toString());
-console.log(printPath(shortestPath(node1, node5)));
-
-let node1a = new WeightedNode('1', []);
-let node2a = new WeightedNode('2', []);
-let node3a = new WeightedNode('3', []);
-let node4a = new WeightedNode('4', []);
-let node5a = new WeightedNode('5', []);
-let node6a = new WeightedNode('6', []);
-
-node1a.addEdge(node2a, 1);
-node1a.addEdge(node6a, 4);
-node2a.addEdge(node3a, 1);
-node3a.addEdge(node4a, 1);
-node4a.addEdge(node5a, 1);
-node5a.addEdge(node6a, 1);
-
-let grapha = new WeightedGraph([node1a, node2a, node3a, node4a, node5a, node6a]);
-
-console.log(grapha.toString());
-console.log(printPath(shortestPath(node1a, node6a)));
-
-let node1b = new WeightedNode('1', []);
-let node2b = new WeightedNode('2', []);
-let node3b = new WeightedNode('3', []);
-let node4b = new WeightedNode('4', []);
-let node5b = new WeightedNode('5', []);
-let node6b = new WeightedNode('6', []);
-
-node1b.addEdge(node2b, 1);
-node1b.addEdge(node6b, 10);
-node2b.addEdge(node3b, 1);
-node3b.addEdge(node4b, 1);
-node4b.addEdge(node5b, 1);
-node5b.addEdge(node1b, 1);
-
-let graphb = new WeightedGraph([node1b, node2b, node3b, node4b, node5b, node6b]);
-
-console.log(graphb.toString());
-console.log(printPath(shortestPath(node1b, node6b)));
-
-let basicWorld = new SimpleWorld([['r', 'x', 'x'],['r', 'r', 'r'],['r', 'x', 'r'], ['r', 'x', 'r'], ['x', 'x', 'r']], {x: 0, y: 0});
-let startNode = new WorldNode('a', [], basicWorld);
-let goalNode = new WorldNode('b', [], new SimpleWorld([], {x: 2, y: 4}));
-console.log(printWorldPath(shortestPath(startNode, goalNode)));
-
-let world = [
-    ['r','r','r','r','r'],
-    ['r','x','x','x','r'],
-    ['r','r','r','x','r'],
-    ['x','x','r','x','r'],
-    ['x','x','r','r','r']
-]
-
-let lessBasicWorld = new SimpleWorld(world, {x:2, y:2});
-let lessBasicStartNode = new WorldNode('a', [], lessBasicWorld);
-let goalOne = new WorldNode('b', [], new SimpleWorld([], {x:0, y:0}));
-let goalTwo = new WorldNode('b', [], new SimpleWorld([], {x:4, y:1}));
-
-console.log(printWorldPath(shortestPath(lessBasicStartNode, goalOne)));
-console.log(printWorldPath(shortestPath(lessBasicStartNode, goalTwo)));
-
-
 let room1: Room = new Room(
     'a',
     ObjectType.NOTHING,
@@ -772,32 +445,6 @@ room6.connections = [
         direction: Direction.WEST
     }
 ]
-
-let testRobot = new Robot(ObjectType.NOTHING, room1);
-let endRobot = new Robot(undefined, room4);
-
-let testRobot1 = new Robot(ObjectType.NOTHING, room1);
-let endRobot1 = new Robot(ObjectType.ORB, undefined);
-
-let testRobot2 = new Robot(ObjectType.NOTHING, room1);
-let endRobot2 = new Robot(ObjectType.ORB, room6);
-
-let testRobot3 = new Robot(ObjectType.NOTHING, room1);
-let endRobot3 = new Robot(undefined, new Room('e', ObjectType.ORB, []));
-
-console.log(shortestPath(testRobot, endRobot));
-console.log(shortestPath(testRobot1, endRobot1));
-console.log(shortestPath(testRobot2, endRobot2));
-console.log(shortestPath(testRobot3, endRobot3));
-
-let severalMovesWithOneRobot = new Robot(ObjectType.NOTHING, room1);
-let firstGoal = new Robot(undefined, room3);
-let secondGoal = new Robot(undefined, room6);
-
-let firstPath = shortestPath(severalMovesWithOneRobot, firstGoal);
-let secondPath = shortestPath(firstPath[firstPath.length - 1].to, secondGoal);
-console.log(firstPath);
-console.log(secondPath);
 
 function createRoomGrid(initialRoom: Room) {
     // Keep track of max/min indexes so we can normalize them later
@@ -986,7 +633,9 @@ function printRoom(room: Room) {
 
 function init() {
     console.log("I am init!")
+    
     // Create map
+    createMap();
     // Paint map
 }
 
